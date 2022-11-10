cfc1cfb59a82fdfef9b83dd9679f5c5a92b8001f
A <abdibekele94@gmail.com>
2022-11-10 10:46:28 -0500

    Add dynamically generated lists

--- src/index.js
+++ src/index.js
@@ -4,45 +4,45 @@ import './style.css';
 //                          
 
 
-// const list = document.querySelector('.list-item');
+const list = document.querySelector('.list-item');
 
-// const tasks = [
-//   {
-//     index: 0,
-//     description: 'Do sport',
-//     completed: true,
-//   },
-//   {
-//     index: 1,
-//     description: 'Do project in time',
-//     completed: true,
-//   },
-//   {
-//     index: 2,
-//     description: 'Coding challenges',
-//     completed: true,
-//   },
-// ];
+const tasks = [
+  {
+    index: 0,
+    description: 'Do sport',
+    completed: true,
+  },
+  {
+    index: 1,
+    description: 'Do project in time',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'Coding challenges',
+    completed: true,
+  },
+];
 
-// const render = (task) => {
-//   const div = document.createElement('div');
-//   div.innerHTML = `
-//     <h3>
-//         <input type="checkbox">
-//             ${task.description}
-//     </h3>
-//         <div>
-//             <i class="fa-solid fa-ellipsis-vertical"></i>
-//         </div>
-//     `;
-//   div.classList.add('list');
-//   list.appendChild(div);
-// };
+const render = (task) => {
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <h3>
+        <input type="checkbox">
+            ${task.description}
+    </h3>
+        <div>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+    `;
+  div.classList.add('list');
+  list.appendChild(div);
+};
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   tasks.sort((a, b) => a.index - b.index)
-//     .map((item) => render(item));
-// });
+document.addEventListener('DOMContentLoaded', () => {
+  tasks.sort((a, b) => a.index - b.index)
+    .map((item) => render(item));
+});
 
 
 
