7d174cd41439ef8882d028e548324e595164429f
A <abdibekele94@gmail.com>
2022-11-17 16:46:51 -0500

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,49 +1,38 @@
 // import _ from 'lodash';
-import { constant, set } from 'lodash';
+// import { constant, set } from 'lodash';
 import './style.css';
-import {lists,addItems,inputValue} from './modules/variables.js'
-import Todo from './modules/Todo.js'
-import {tasks} from './modules/Todo.js'
+import { lists, addItems, inputValue } from './modules/variables.js';
+import Todo, { tasks } from './modules/Todo.js';
 // import {deleteToDo,editTodo} from './modules/crud.js'
 
-
-addItems.addEventListener('submit',function(e)
-{
- 
+addItems.addEventListener('submit', (e) => {
   e.preventDefault();
   Todo.saveTodo();
   Todo.renderToDo();
-  localStorage.setItem('tasks',JSON.stringify(tasks));
-})
-lists.onclick=(e)=>
-{  const { target } = e;
-const parentEle = target.parentNode;
-const item = parentEle;
-if (parentEle.className !== 'item') return;
-const targetId = Number(item.id);
- const action =target.dataset.action;
-console.log("id",targetId)
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+});
+lists.onclick = (e) => {
+  const { target } = e;
+  const parentEle = target.parentNode;
+  const item = parentEle;
+  if (parentEle.className !== 'item') return;
+  const targetId = Number(item.id);
+  const { action } = target.dataset;
 
-action ==='delete' &&  deleteToDo(targetId);
-action ==='edit' && editTodo(targetId);
+  npxaction === 'delete' && deleteToDo(targetId);
+  action === 'edit' && editTodo(targetId);
+};
+function deleteToDo(targetId) {
+  tasks.splice(targetId, 1);
+  tasks.forEach((todo, index) => {
+    todo.index = index + 1;
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 
-
-}
-function deleteToDo (targetId)
-  {
-tasks.splice(targetId,1)
-tasks.forEach((todo, index) => {
-  todo.index = index + 1;})
-   localStorage.setItem('tasks',JSON.stringify(tasks)); 
-  
   Todo.renderToDo();
-
-  }
-function editTodo(targetId)
-  {
-    console.log("edit button toucheed")
-    inputValue.value=tasks[targetId].description;
-    deleteToDo(targetId);
- 
-  }
+}
+function editTodo(targetId) {
+  inputValue.value = tasks[targetId].description;
+  deleteToDo(targetId);
+}
 Todo.renderToDo();
--- src/modules/crud.js
+++ src/modules/crud.js
@@ -1,40 +1,19 @@
-import Todo from './Todo.js'
-import {lists,addItems,inputValue} from './variables.js'
-import {tasks} from './Todo.js'
+import Todo, { tasks } from './Todo.js';
+import { inputValue } from './variables.js';
 
-export function deleteToDo (targetId)
-  {
-tasks.splice(targetId,1)
-tasks.forEach((todo, index) => {
-  todo.target = index + 1;})
-console.log(tasks);
-   localStorage.setItem('tasks',JSON.stringify(tasks)); 
-  
+export function deleteToDo(targetId) {
+  tasks.splice(targetId, 1);
+  tasks.forEach((todo, index) => {
+    todo.target = index + 1;
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
   Todo.renderToDo();
-
-  }
-  export function editTodo(targetId)
-  {
-    console.log("edit button toucheed")
-    inputValue.value=tasks[targetId].description;
-    deleteToDo(targetId);
- 
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+export function editTodo(targetId) {
+  inputValue.value = tasks[targetId].description;
+  deleteToDo(targetId);
+}
 
 //   }
