80c546f2b769fd172a3a50a08f58137d07287a83
A <abdibekele94@gmail.com>
2022-11-19 03:31:46 -0500

    Add functionality method to delete completed tasks

--- src/index.js
+++ src/index.js
@@ -24,4 +24,46 @@ lists.onclick = (e) => {
   if (action === 'edit' && editTodo(targetId)) ;
   if(action === 'check' && checkToDo(targetId));
 };
-Todo.renderToDo();
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click',(e)=>
+{
+  e.preventDefault();
+  console.log("Im joining here");
+   const completedArray = JSON.parse(localStorage.getItem('tasks'));
+    console.log(completedArray);
+  const gonnaCompleted = completedArray.filter((todo) => todo.checked === false);
+  console.log(gonnaCompleted);
+  localStorage.setItem('tasks', JSON.stringify(gonnaCompleted));
+  window.location.reload();
+  Todo.renderToDo();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const clear = document.getElementById('clear');
+// clear.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   console.log("Im joining here");
+//   const list = JSON.parse(localStorage.getItem('tasks'));
+//   const todoArr = list.filter((todo) => todo.completed === false);
+//   todoArr.forEach((todo) => {
+//     todo.index = 1 + todoArr.indexOf(todo);
+//   });
+
+//   localStorage.setItem('tasks', JSON.stringify(todoArr));
+//   // window.location.reload();
+// });
